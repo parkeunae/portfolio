b8199dc5d511405e7a711d106e1dc37b722c31c2
parkeunae <eapark108@gmail.com>
2020-12-24 18:03:07 +0900

    chore: change the setting with react + typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Root = () => {
     return (
-        <h1>CRA 없이 React 개발하기</h1>
+        <h1>CRA 없이 React 개발하기 + typescript</h1>
     );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,48 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    devtool: 'eval',
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
+module.exports = (webpackEnv) => {
+    const isDevelopment = webpackEnv === 'development' ? true : false;
 
-    entry: {
-        bundle: ['./src/index']
-    }, // 입력
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: '/node_modules',
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-            }
-        }]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        }),
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].js',
-    }, // 출력
-    devServer: {
-        publicPath: '/',
-        hot: true,
-        open: true,
+    return {
+        mode: 'development',
+        devtool: 'eval',
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js' ]
+        },
+    
+        entry: {
+            bundle: ['./src/index.tsx']
+        }, // 입력
+        module: {
+            rules: [{
+                test: /\.tsx?$/,
+                exclude: '/node_modules',
+                use: [
+                    'cache-loader',
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: isDevelopment ? true : false,
+                        }
+                    }
+                ]
+            }]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: './public/index.html'
+            }),
+        ],
+        output: {
+            path: path.join(__dirname, 'dist'),
+            filename: '[name].js',
+        }, // 출력
+        devServer: {
+            publicPath: '/',
+            hot: true,
+            open: true,
+        }
     }
 }

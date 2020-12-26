fe69cf49464cc136d8f57e1f38cb4c93dc226b9f
parkeunae <eapark108@gmail.com>
2020-12-26 23:56:14 +0900

    feat: add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                Front-end Developer
+            </h1>
+            <p >
+                안녕하세요, 프론트엔드 개발자 박은애입니다.
+            </p>
+            <button>
+                See my portfolio ▼
+            </button>
+        </header>
+    )
+}
+
+export default Header;
